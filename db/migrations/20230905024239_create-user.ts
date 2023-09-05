42ec83b817994283247d38988aceff5cd55c31bb
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-09-05 00:27:47 -0300

    erro no eslint corrigido

---
+++ db/migrations/20230905024239_create-user.ts
@@ -0,0 +1,10 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('name').nullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {}
