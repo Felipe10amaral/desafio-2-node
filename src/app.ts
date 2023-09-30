4a788dbc34caf77903871e54171bd47e8969e92b
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-09-30 19:23:18 -0300

    adicionando cookies

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
+import cookies from '@fastify/cookie'
 import { usersRoutes } from './routes/userRoutes'
 import { snacksRoutes } from './routes/snackRoutes'
 
 export const app = fastify()
+
+app.register(cookies)
 
 app.register(usersRoutes)
 app.register(snacksRoutes)
