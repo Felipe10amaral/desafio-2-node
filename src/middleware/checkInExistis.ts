e49b9f98538d019b0b10599d496b0477f2df1964
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-10-02 12:28:11 -0300

    criando middleware de checkin

---
+++ src/middleware/checkInExistis.ts
@@ -0,0 +1,9 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export function checkInExists(request: FastifyRequest, reply: FastifyReply) {
+  const sessionID = request.cookies.session_id
+
+  if (!sessionID) {
+    return reply.status(401).send({ error: 'Unauthorized checkin' })
+  }
+}
